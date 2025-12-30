9580f2150712e5d12aee7a2776c43893077b649f
supriseeeeef <romankorniiets@gmail.com>
2025-12-30 21:44:48 +0200

    useRef + useEffect

--- my-heaphones-magazin/src/App.jsx
+++ my-heaphones-magazin/src/App.jsx
@@ -5,16 +5,19 @@ import HeadphonesList from "./components/HeadphonesList";
 function App() {
   const [maxPr, setPr] = useState(2000);
 
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, [maxPr]);
+
   return (
     <div>
       <h2>Навушники</h2>
       <label>
         Максимальна ціна:
-        <input
-          type="number"
-          value={maxPr}
-          onChange={e => setPr(Number(e.target.value))}
-        />
+        <input type="number" ref={ref} value={maxPrice} onChange={e => setPr(Number(e.target.value))} />
       </label>
 
       <HeadphonesList products={products} maxPrice={maxPr} />
